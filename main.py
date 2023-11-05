81b4582552ddbaf94afc27b37a0eb170bf04bac1
andynic <andysnic@hotmail.com>
2023-11-05 14:41:27 +0000

    editing images simple overexposeure detection

--- main.py
+++ main.py
@@ -1,16 +1,56 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from PIL import Image
+import numpy as np
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def is_overexposed(pixel):
+    for i in range(3):
+        if pixel[i] > 220:
+            return True
+    return False
 
 
-# Press the green button in the gutter to run the script.
+def process(numpydata):
+    height = numpydata.shape[0]
+    width = numpydata.shape[1]
+    out = np.zeros((height, width, 3)).astype('uint8')
+    tot_count = width * height
+    count = 0
+    for line in range(height):
+        print(str(count / tot_count * 100) + "%")
+        for column in range(width):
+            count += 1
+            if is_overexposed(numpydata[line][column]):
+                out[line][column] = [255, 0, 0]
+            else:
+                out[line][column] = numpydata[line][column]
+    return out
+
+
+# load the image and convert into
+# numpy array
+def load_test_images():
+    hi_res_1 = Image.open('testing resources/hi_res_1.JPG')
+    hi_res_2 = Image.open('testing resources/hi_res_2.JPG')
+    hi_res_3 = Image.open('testing resources/hi_res_3.JPG')
+    low_res_1 = Image.open('testing resources/low_res_1.JPG')
+    low_res_2 = Image.open('testing resources/low_res_2.JPG')
+    low_res_3 = Image.open('testing resources/low_res_3.JPG')
+    return hi_res_1, hi_res_2, hi_res_3, low_res_1, low_res_2, low_res_3
+
+
+img = load_test_images()
+numpydata = np.asarray(img[3])
+
+out = process(numpydata)
+print(type(numpydata[0][0][0]))
+print(numpydata.shape)
+print(type(out[0][0][0]))
+print(out.shape)
+
+image = Image.fromarray(out, 'RGB')
+# Save the image
+image.save('image.png')
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    pass
